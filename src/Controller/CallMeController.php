8c4c82a1f439d2acd82d4753c1fe4b7ac0072779
David Davó <david@ddavo.me>
2020-03-17 18:46:42 +0100

    Añadido success flash
    Signed-off-by: David Davó <david@ddavo.me>

--- src/Controller/CallMeController.php
+++ src/Controller/CallMeController.php
@@ -15,12 +15,19 @@ class CallMeController extends AbstractController
 
         $form = $this->createForm(OnlineCallType::class, $call);
         $form->handleRequest($request);
+        $entityManager = $this->getDoctrine()->getManager();
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($form->getData());
+            $data = $form->getData();
+            $entityManager->persist($data);
             $entityManager->flush();
 
-            return $this->redirectToRoute('callme_success');
+            $xdarr = [
+                "Oído cocina", "Recibido"
+            ];
+            $this->addFlash('success', "¡{$xdarr[array_rand($xdarr)]}! {$data->getName()}, ¿quieres que le recitemos un poema a alguien más?");
+
+            return $this->redirect($request->getUri());
         }
 
         return $this->render('pxt/index.html.twig', [
