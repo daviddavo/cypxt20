1dc4aef5d3cafaee40e0eef638d11a9f9080dcc8
David Davó <david@ddavo.me>
2020-04-21 13:08:21 +0200

    Contador inscritos

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -15,6 +15,30 @@ const barlayout = {
     bargap :0.05
 };
 
+function plotCnt(ages) {
+    let sum = 0;
+    ages.forEach(function(r) {
+        sum += +r["cnt"];
+    });
+
+    let data = [{
+        type: "indicator",
+        mode: "gauge+number",
+        title: {text: "Inscritos"},
+        value: sum,
+        gauge: {axis: {visible: true, range: [0, 200]}}
+    }];
+
+    let layout = [{
+       font:{
+           family: 'Raleway, sans-serif'
+       }
+    }];
+
+    $('#totalcnt').text(sum);
+    Plotly.newPlot('cntplot', data, layout);
+}
+
 function plotAges(ages) {
     let xarr = []; let yarr = [];
     let sum = 0;
@@ -36,7 +60,6 @@ function plotAges(ages) {
     layout.title = 'Edad de los participantes';
 
     Plotly.newPlot('ageplot', data, layout);
-    $('#totalcnt').text(sum);
 }
 
 function plotHours(hours) {
@@ -73,6 +96,7 @@ $.ajax({
     success: function(result) {
         plotAges(result["ages"]);
         plotHours(result["hours"]);
+        plotCnt(result["ages"]);
     },
     error: function(error) {
         console.error(error)
