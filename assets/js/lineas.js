9f15dcc8b387f30d27dadd54b15f1e38bcd4c374
David Davó <david@ddavo.me>
2021-03-20 12:43:28 +0100

    Terminado panel de control

--- assets/js/lineas.js
+++ assets/js/lineas.js
@@ -1,2 +1,43 @@
 import './global.js';
 import '../css/lineas.scss';
+
+function updateTime() {
+    const now = new Date();
+    $("#table-lineas .linea").each(function () {
+        const status = $(this).data("status");
+        console.log($(this).data());
+        let t;
+        if (status === "idle") {
+            t = new Date($(this).data("last_close"));
+        } else if (status == "busy") {
+            t = new Date($(this).data("last_open"));
+        } else {
+            return;
+        }
+
+        let d = new Date(now.getTime() - t.getTime());
+        let h = d.getTime() / 1000 / 3600 >> 0;
+        h = h < 10 ? ('0'+h) : h;
+        let m = d.getMinutes() < 10 ? ('0'+d.getMinutes()) : (''+d.getMinutes());
+        let s = d.getSeconds() < 10 ? ('0'+d.getSeconds()) : (''+d.getSeconds());
+        $(this).find(".linea-time").html(`${h}:${m}:${s}`)
+    })
+}
+
+function updateData() {
+    $.get("/api/lineas", function(data) {
+        $("#table-lineas .linea").each(function () {
+            const id = $(this).data("id");
+            $(this).data(data[id]);
+            $(this).attr("data-status", data[id].status);
+        });
+    });
+}
+
+$().ready(function () {
+    updateTime();
+    setInterval(updateTime, 1000);
+
+    updateData();
+    setInterval(updateData, 5000);
+});
--- src/Controller/LineasController.php
+++ src/Controller/LineasController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Line;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,10 +23,36 @@ class LineasController extends AbstractController {
     }
 
     /**
+     * @Route("/lineas/{id}", name="linea_id")
+     */
+    public function mainLinea(int $id) {
+        $repo = $this->getDoctrine()->getRepository(Line::class);
+        $linea = $repo->find($id);
+        if (is_null($linea)) {
+            throw $this->createNotFoundException("Linea no encontrada");
+        }
+
+        return $this->render('pxt/lineas.html.twig', [
+            'lineas' => [$repo->find($id)]
+        ]);
+    }
+
+    /**
      * @Route("/api/lineas", name="api_lineas")
      */
     public function api() {
         $repo = $this->getDoctrine()->getRepository(Line::class);
-        return new JsonResponse($repo->getAll());
+        $r = [];
+        foreach ($repo->findAll() as $item) {
+            $r[$item->getId()] = [
+                'id' => $item->getId(),
+                'description' => $item->getDescription(),
+                'status' => $item->getStatus(),
+                'last_open' => $item->getLastOpen()->format(DateTime::ISO8601),
+                'last_close' => $item->getLastClose()->format(DateTime::ISO8601),
+                'phone_number' => $item->getPhoneNumber(),
+            ];
+        }
+        return new JsonResponse($r);
     }
 }
