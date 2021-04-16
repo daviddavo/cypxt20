635a646d096cf18a38c736e2452580f87b4a9ff5
David Davó <david@ddavo.me>
2021-04-16 11:51:21 +0200

    Continuando

--- src/Controller/CallMeController.php
+++ src/Controller/CallMeController.php
@@ -14,14 +14,31 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CallMeController extends AbstractController
 {
+    private function getParamsFromHostname(Request $request) {
+        $subdomain = explode('.', $request->getHost())[0];
+        return $this->getParameter($subdomain=='cxt'?'app.cxt':'app.pxt');
+    }
+
     /**
      * @Route("/", name="home")
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function main(Request $request) {
+        $params = $this->getParamsFromHostname($request);
+
+        return $this->render('pxt/welcome.html.twig', [
+            'title' => $params['title'],
+            'cypxt_params' => $params
+        ]);
+    }
+
+    /**
+     * @Route("/form", name="form")
+     * @return Response
+     */
+    public function form(Request $request) {
         $call = new OnlineCall();
-        $subdomain = explode('.', $request->getHost())[0];
-        $configparams = $this->getParameter($subdomain=='cxt'?'app.cxt':'app.pxt');
+        $configparams = $this->getParamsFromHostname($request);
 
         $form = $this->createForm(OnlineCallType::class, $call, [
             'singular' => $configparams['cop_sing'],
@@ -38,7 +55,7 @@ class CallMeController extends AbstractController
             $xdarr = [
                 "Oído cocina", "Recibido", "Marchando"
             ];
-            $this->addFlash('success', "¡{$xdarr[array_rand($xdarr)]}! {$data->getFromName()}, ¿quieres que le " . (($subdomain=='cxt')?'contemos un cuento':'recitemos un poema') . " a alguien más?");
+            $this->addFlash('success', "¡{$xdarr[array_rand($xdarr)]}! {$data->getFromName()}, ¿quieres que le " . $configparams['cop_phrase_question'] . " a alguien más?");
 
             return $this->redirect($request->getUri());
         }
