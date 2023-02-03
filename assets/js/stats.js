bb9f80b6370959ed5a323ef129beb41999840872
David Davó <david@ddavo.me>
2023-02-03 22:21:32 +0100

    Added dark mode to stats

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,25 +1,22 @@
 import Plotly from 'plotly.js-dist';
+import { dark_template } from './stats/dark-template';
 
-const commonLayout = {
-    font:{
-        family: 'Raleway, sans-serif'
-    },
-    paper_bgcolor: 'rgba(0,0,0,0)',
-    plot_bgcolor: 'rgba(0,0,0,0)'
-};
 
-const barlayout = {
-    showlegend: false,
-    xaxis: {
-        tickangle: -45
-    },
-    yaxis: {
-        zeroline: false,
-        gridwidth: 2
-    },
-    bargap :0.05,
-    ...commonLayout
-};
+
+function isDarkMode() {
+    return document.body.style.colorScheme == "dark";
+}
+
+function getCommonLayout() {
+    return {
+        font:{
+            family: 'Raleway, sans-serif'
+        },
+        paper_bgcolor: 'rgba(0,0,0,0)',
+        plot_bgcolor: 'rgba(0,0,0,0)',
+        template: isDarkMode()?dark_template:{},
+    };
+}
 
 function plotCnt(ages, expected) {
     let sum = 0;
@@ -37,7 +34,9 @@ function plotCnt(ages, expected) {
 
     $('#totalcnt').text(sum);
 
-    let layout = {...commonLayout};
+    let layout = {...getCommonLayout()};
+    
+    console.log("Using layout:", layout);
     Plotly.newPlot('cntplot', data, layout);
 }
 
@@ -60,11 +59,31 @@ function plotAges(ages) {
 
     let layout = {
         title: 'Edad de los participantes',
-        ...barlayout
+        showlegend: false,
+        xaxis: {
+            tickangle: -45
+        },
+        yaxis: {
+            zeroline: false,
+            gridwidth: 2
+        },
+        bargap :0.05,
+        ...getCommonLayout(),
     };
 
     Plotly.newPlot('ageplot', data, layout);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutationRecord) {
+            let cl = getCommonLayout();
+            Plotly.relayout('cntplot', cl);
+            Plotly.relayout('ageplot', cl);
+        })
+    });
+    observer.observe(document.body, {attributes: true, attributeFilter: ['style']});
+});
 
 $.ajax({
     url: "/api/stats",
